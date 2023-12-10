5206d6a4e0c0d7072b540ba81846fb89dca96f2e
Deepkha <deepeshkhatri902@gmail.com>
2023-12-10 11:33:29 +0530

    crud operation fixed

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+
 const app = express();
 const PORT = 8000;
 
-// app.use(express.json()); // Use express.json() for parsing JSON requests
+
 
 app.use(express.urlencoded({extended: false}));
 mongoose
-  .connect("mongodb://127.0.0.1:27017/code")
+  .connect("mongodb://127.0.0.1:27017/codes")
   .then(() => console.log("Connected successfully"))
   .catch((err) => console.log("Mongoose error:", err));
 
-const userSchema = new mongoose.Schema({
-    NumId:{
-        type:Number,
-        unique:true,
-        required:true,
+  const userSchema = new mongoose.Schema({
+    NumId: {
+      type: Number,
+     
     },
-  firstName: {
-    type: String,
-    required: true,
+    firstName: {
+      type: String,
+      
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  jobTitle: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-},
-{timestamps:true}
-);
+  {timestamps:true}
+  );
+  
 
 const User = mongoose.model("user", userSchema);
 
@@ -49,7 +48,7 @@ app.get("/api/users", async (req, res) => {
 
 app.get("/api/users/:id", async (req, res) => {
   const uid = Number(req.params.id);
-  const user = await User.findById(uid );
+  const user = await User.findById(uid);
   if(!user){
     return res.status(400).json({error:"user not found"});
   }
@@ -59,37 +58,46 @@ app.get("/api/users/:id", async (req, res) => {
 
 
 app.post("/api/users", async (req, res) => {
+ 
   const body = req.body;
   if ( !body ||
-     !body.firstName ||
-      !body.lastName || 
-      !body.gender ||
-      !body.email || 
-      !body.jobTitle ||
-      !body.NumId ) {
+    !body.firstName ||
+     !body.lastName || 
+     !body.gender ||
+     !body.email || 
+     !body.jobTitle ||
+     !body.NumId ) {
 
-    return res.status(400).json({ msg: "All fields should be filled" });    
+   return res.status(400).json({ msg: "All fields should be filled" });    
+ }
+  const contact=new User({
+      NumId: body.NumId,
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email,
+      gender: body.gender,
+      jobTitle: body.jobTitle,
+
+  })
+  try {
+    
+
+    const news=await contact.save();
+    res.json(news);
+   
+  } catch (error) {
+    console.error("Failed to save contact", error);
+    res.status(500).json({msg:"error"});
   }
 
-  const result = await User.create(
-    {
-     NumId:body.NumId,
-    firstName: body.firstName,
-    lastName: body.lastName,
-    email: body.email,
-    gender: body.gender,
-    jobTitle: body.jobTitle,
-  })
-
-  return res.status(201).json({ msg: "Success" });
 });
 app.patch("/api/users/:id",async (req,res) =>{
-    await User.findByIdNumAndUpdate(req.params.id,{ lastName:"Bisht"});
+    await User.findByIdAndUpdate(req.params.id,{ lastName:"Bisht"});
     res.status(201).json({ status : "changed" });
 })
 app.delete("/api/users/:id",async(req,res) =>{
     try{
-      await User.findByIdNumAndDelete(req.params.id);
+      await User.findByIdAndDelete(req.params.id);
     return res.json({status :"Susccess"});
     }
     catch{
